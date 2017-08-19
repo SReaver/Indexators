a63535736d0f912e00f9c2966f792e19327f79c0
Soul Reaver <s.reaver@outlook.com>
2017-08-19 11:56:00 +0600

    Done

--- Indexators/Program.cs
+++ Indexators/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace Indexators
 {
-    class Program
+    public class Indexator
     {
-        static void Main(string[] args)
+        int[] arr;
+        public int Length;
+        public int this[int index]
         {
+            get { return arr[index]; }
+            set { arr[index] = value * value; }
+        }
+        public Indexator(int Size)
+        {
+            arr = new int[Size];
+            Length = Size;
         }
     }
+
+
+    class Program
+    {
+        static void Main()
+        {
+            Indexator i1 = new Indexator(10);
+            for (int i = 0; i < i1.Length; i++)
+            {
+                i1[i] = i;
+                Console.WriteLine(i1[i]);
+            }
+        }
+    }
+
 }
